eb0160e0eff2a7ba38555605e43d92f02ac9ad7c
ronengoren <ronen.goren@gmail.com>
2022-10-16 19:07:22 -0400

    changed base url routes58

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -61,9 +61,7 @@ const Home = () => {
               className="mt-2 inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-left text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:border-gray-200
           disabled:bg-white disabled:text-gray-900 disabled:focus:outline-none disabled:dark:border-gray-600 disabled:dark:bg-gray-800 disabled:dark:text-gray-400 sm:text-sm"
             >
-              {/* {isCopyButtonEnabled && (
-                    <DuplicateIcon className="mr-2 h-6 w-6 cursor-pointer dark:stroke-white" />
-                )} */}
+           
               Create New Game
             </button>
           </Link>
@@ -78,28 +76,14 @@ const Home = () => {
               className="mt-2 inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-2 text-left text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:border-gray-200
           disabled:bg-white disabled:text-gray-900 disabled:focus:outline-none disabled:dark:border-gray-600 disabled:dark:bg-gray-800 disabled:dark:text-gray-400 sm:text-sm"
             >
-              {/* {isCopyButtonEnabled && (
-                    <DuplicateIcon className="mr-2 h-6 w-6 cursor-pointer dark:stroke-white" />
-                )} */}
+            
               Your Wordles
             </button>
           </Link>
         </div>
-        {/* <div className="flex justify-center">
-          <h1>Your Wordles:</h1>
-        </div>
-        {sentWords ? sentWords.map((i =>
-          <div key={i} className="mx-auto flex pt-2 pb-8 sm:px-6 md:max-w-7xl lg:px-8 short:pb-2 short:pt-2">
-            {unicodeSplit(i).map((letter, h) => <div
-              key={h}
-              className={classes}
-              style={{ animationDelay }}
-            > <div className="letter-container" style={{ animationDelay }}> <h1>{letter}</h1> </div></div>)}
+       
 
-
-          </div>)) : null} */}
-
-        <Link to={`/wordle?getword=RONEN`}>Wordle</Link>
+        <Link to={`/wordle`}>Wordlsdsde</Link>
 
       </div>
 
