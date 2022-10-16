361c56f3fa448a4bfe0b934e22e2f7f8a3d15510
ronengoren <ronen.goren@gmail.com>
2022-10-16 19:55:06 -0400

    Updates

---
+++ precache-manifest.813f1d84d415800750876e109adc8b74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "df655ffa355f1521c7eb",
+    "url": "/Wgame/static/css/main.2cecc32d.chunk.css"
+  },
+  {
+    "revision": "df655ffa355f1521c7eb",
+    "url": "/Wgame/static/js/main.df655ffa.chunk.js"
+  },
+  {
+    "revision": "21cce7341b4f836f50f8",
+    "url": "/Wgame/static/js/1.21cce734.chunk.js"
+  },
+  {
+    "revision": "c179381c707ac664e22a",
+    "url": "/Wgame/static/js/2.c179381c.chunk.js"
+  },
+  {
+    "revision": "8ef995f0d90a469b0f42",
+    "url": "/Wgame/static/js/runtime~main.8ef995f0.js"
+  },
+  {
+    "revision": "5c9ff488542baa9964873efdf2463040",
+    "url": "/Wgame/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Wgame/precache-manifest.bb1b3e3949a73304d9fdc044530f1a3d.js"
+  "/Wgame/precache-manifest.813f1d84d415800750876e109adc8b74.js"
 );
 
 workbox.clientsClaim();
