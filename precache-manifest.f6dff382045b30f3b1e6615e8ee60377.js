eb97832dd64784cfa3794cddb09464ffaea01e9a
ronengoren <ronen.goren@gmail.com>
2022-10-16 13:15:53 -0400

    Updates

---
+++ precache-manifest.f6dff382045b30f3b1e6615e8ee60377.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "44e002b6833bbbc4979e",
+    "url": "/Wgame/static/css/main.7e9b01c7.chunk.css"
+  },
+  {
+    "revision": "44e002b6833bbbc4979e",
+    "url": "/Wgame/static/js/main.44e002b6.chunk.js"
+  },
+  {
+    "revision": "21cce7341b4f836f50f8",
+    "url": "/Wgame/static/js/1.21cce734.chunk.js"
+  },
+  {
+    "revision": "1c2e7445f0de57fdb41e",
+    "url": "/Wgame/static/js/2.1c2e7445.chunk.js"
+  },
+  {
+    "revision": "8ef995f0d90a469b0f42",
+    "url": "/Wgame/static/js/runtime~main.8ef995f0.js"
+  },
+  {
+    "revision": "1e12ca1018d15e1aa1c2f65791290baa",
+    "url": "/Wgame/index.html"
+  }
+];
