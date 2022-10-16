6d2588617a81052e630a00de01dd8afb944c4687
ronengoren <ronen.goren@gmail.com>
2022-10-16 18:24:30 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Wgame/precache-manifest.263b73b47ab4a41c0e75eebebfc8adcc.js"
+  "/Wgame/precache-manifest.6a342f02606046b6781a39f0d89b0e98.js"
 );
 
 workbox.clientsClaim();
