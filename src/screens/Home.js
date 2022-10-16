492082052161d74118221965da5467f9a5e36440
ronengoren <ronen.goren@gmail.com>
2022-10-16 16:33:56 -0400

    changed base url routes27

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -12,8 +12,8 @@ import { REVEAL_TIME_MS } from '../constants/settings';
 import { getNewWord } from '../lib/localStorage';
 
 const Home = () => {
-  console.log('=======Home11=============================');
-  console.log('Home11');
+  console.log('=======Home12=============================');
+  console.log('Home12');
   const [isInfoModalOpen, setIsInfoModalOpen] = useState(false);
   const [isStatsModalOpen, setIsStatsModalOpen] = useState(false);
   const [isDatePickerModalOpen, setIsDatePickerModalOpen] = useState(false);
--- src/screens/Wordle.js
+++ src/screens/Wordle.js
@@ -25,8 +25,8 @@ import { getStoredIsHighContrastMode, loadGameStateFromLocalStorage, saveGameSta
 import { addStatsForCompletedGame, loadStats } from '../lib/stats';
 import { findFirstUnusedReveal, getGameDate, getIsLatestGame, isWinningWord, isWordInWordList, setGameDate, solution, solutionGameDate, unicodeLength, } from '../lib/words';
 function Wordle() {
-  console.log('=======Wordle10=============================');
-  console.log('Wordle10');
+  console.log('=======Wordle11=============================');
+  console.log('Wordle11');
 
     const isLatestGame = getIsLatestGame();
     const gameDate = getGameDate();
